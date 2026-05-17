62a2ef6c27a6a205465a621f811d8cd5ec195576
rim-btw <colizeirim@gmail.com>
2026-05-18 02:47:16 +0300

    Update Задача-2.c

--- Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-2.c
+++ Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-2.c
@@ -4,6 +4,10 @@ int main() {
     int a, b;
     int *pa = &a, *pb = &b;
     scanf("%d %d", pa, pb);
-    printf("%d\n", *pa + *pb);
+    printf("%d %d\n", *pa, *pb);
+    int temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+    printf("%d %d\n", *pa, *pb);
     return 0;
 }
