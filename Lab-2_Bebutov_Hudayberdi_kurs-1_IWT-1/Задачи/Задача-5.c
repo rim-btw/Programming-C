2ad0fbfd4a43a84150e73be8ca50e8e7b4fbf2f3
rim-btw <colizeirim@gmail.com>
2026-05-18 02:48:23 +0300

    Update Задача-5.c

--- Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-5.c
+++ Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-5.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int a, b;
-    int *pa = &a, *pb = &b;
-    scanf("%d %d", pa, pb);
-    printf("%d\n", *pa + *pb);
+    int n;
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    if (!arr) return 1;
+    int *ptr = arr;
+    for (int i = 0; i < n; i++, ptr++)
+        scanf("%d", ptr);
+    ptr = arr + n - 1;
+    for (int i = 0; i < n; i++, ptr--)
+        printf("%d ", *ptr);
+    printf("\n");
+    free(arr);
     return 0;
 }
