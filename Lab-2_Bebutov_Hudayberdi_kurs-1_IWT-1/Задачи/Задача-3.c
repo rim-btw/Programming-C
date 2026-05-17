f17092314020668d80ab6934acaf3a0933d11dc1
rim-btw <colizeirim@gmail.com>
2026-05-18 02:47:42 +0300

    Update Задача-3.c

--- Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-3.c
+++ Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-3.c
@@ -4,6 +4,7 @@ int main() {
     int a, b;
     int *pa = &a, *pb = &b;
     scanf("%d %d", pa, pb);
-    printf("%d\n", *pa + *pb);
+    int max = (*pa > *pb) ? *pa : *pb;
+    printf("%d\n", max);
     return 0;
 }
