8ab2efbb9f4186b50ffe7eb17194b6d2b842489f
rim-btw <colizeirim@gmail.com>
2026-05-18 02:48:40 +0300

    Update Задача-6.c

--- Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-6.c
+++ Lab-2_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-6.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    int *pa = &a, *pb = &b;
-    scanf("%d %d", pa, pb);
-    printf("%d\n", *pa + *pb);
+    int a = 1234567890;
+    unsigned char *ptr = (unsigned char*)&a;
+    for (int i = 0; i < sizeof(a); i++)
+        printf("%d ", ptr[i]);
+    printf("\n");
     return 0;
 }
