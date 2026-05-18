715b5b3c1e2957fb72d8feb8a7fa165396c8903d
rim-btw <colizeirim@gmail.com>
2026-05-18 06:29:20 +0300

    Update Задача-7.c

--- Lab-1_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-7.c
+++ Lab-1_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-7.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int rows = 2, cols = 3;
-    int **matrix = (int**)malloc(rows * sizeof(int*));
-    for (int i = 0; i < rows; i++)
-        matrix[i] = (int*)malloc(cols * sizeof(int));
-
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            scanf("%d", &matrix[i][j]);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++)
-            printf("%d ", matrix[i][j]);
-        printf("\n");
+    int n;
+    int arr[100];
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
     }
-
-    for (int i = 0; i < rows; i++)
-        free(matrix[i]);
-    free(matrix);
+    
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    
+    printf("\n");
     return 0;
 }
