37e9d9d014fa7f0afa2b5e6038731ddccef3c2f4
rim-btw <colizeirim@gmail.com>
2026-05-18 05:57:39 +0300

    Update Задача-1.4.c

--- Lab-4_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-1.4.c
+++ Lab-4_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-1.4.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
-struct Date {
-    unsigned int day   : 5;
-    unsigned int month : 4;
-    unsigned int year  : 11;
-};
+void cross3(const double a[3], const double b[3], double out[3])
+{
+    out[0] = a[1] * b[2] - a[2] * b[1];
+    out[1] = a[2] * b[0] - a[0] * b[2];
+    out[2] = a[0] * b[1] - a[1] * b[0];
+}
 
-int main() {
-    struct Date birthday;
-    int d, m, y;
+double dot3(const double a[3], const double b[3])
+{
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
 
-    scanf("%d %d %d", &d, &m, &y);
+double triple3(const double a[3], const double b[3], const double c[3])
+{
+    double cross_result[3];
+    cross3(b, c, cross_result);
+    return dot3(a, cross_result);
+}
 
-    birthday.day = d;
-    birthday.month = m;
-    birthday.year = y;
-
-    printf("%u %u %u\n", birthday.day, birthday.month, birthday.year);
-    printf("%zu\n", sizeof(birthday));
-
+int main()
+{
+    double a[3], b[3], c[3];
+    
+    scanf("%lf %lf %lf", &a[0], &a[1], &a[2]);
+    scanf("%lf %lf %lf", &b[0], &b[1], &b[2]);
+    scanf("%lf %lf %lf", &c[0], &c[1], &c[2]);
+    
+    double result = triple3(a, b, c);
+    
+    printf("%.0lf\n", result);
+    
     return 0;
 }
