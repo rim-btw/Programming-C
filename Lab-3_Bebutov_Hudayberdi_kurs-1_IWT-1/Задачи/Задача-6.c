6b9536641cdcd038c97acd6b92aaca03c1c98e35
rim-btw <colizeirim@gmail.com>
2026-05-18 05:04:04 +0300

    Update Задача-6.c

--- Lab-3_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-6.c
+++ Lab-3_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-6.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 
+union Data {
+    int i;
+    float f;
+    char str[20];
+};
+
 int main() {
-    int a = 1234567890;
-    unsigned char *ptr = (unsigned char*)&a;
-    for (int i = 0; i < sizeof(a); i++)
-        printf("%d ", ptr[i]);
-    printf("\n");
+    union Data data;
+    union Data *ptr = &data;
+    int choice;
+
+    ptr->i = 42;
+    printf("int: %d\n", ptr->i);
+
+    ptr->f = 3.14159;
+    printf("float: %f\n", ptr->f);
+
+    sprintf(ptr->str, "Hello, Union!");
+    printf("string: %s\n", ptr->str);
+
+    printf("\nВыберите тип для вывода (1=int, 2=float, 3=string): ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+        printf("int: %d\n", ptr->i);
+    else if (choice == 2)
+        printf("float: %f\n", ptr->f);
+    else if (choice == 3)
+        printf("string: %s\n", ptr->str);
+    else
+        printf("Неверный выбор\n");
+
     return 0;
 }
