319cd29f64bf60a30aeeca9640268fca5f2aa15a
rim-btw <colizeirim@gmail.com>
2026-05-18 06:27:27 +0300

    Update Задача-2.c

--- Lab-1_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-2.c
+++ Lab-1_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-2.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    int *pa = &a, *pb = &b;
-    scanf("%d %d", pa, pb);
-    printf("%d %d\n", *pa, *pb);
-    int temp = *pa;
-    *pa = *pb;
-    *pb = temp;
-    printf("%d %d\n", *pa, *pb);
+    double x, y, u;
+    scanf("%lf %lf", &x, &y);
+    u = (x + y) / 2.0 + x * y;
+    printf("%.3lf\n", u);
     return 0;
 }
