874de63cc04cb3ec10323e56857642e7b534315d
rim-btw <colizeirim@gmail.com>
2026-05-18 06:12:45 +0300

    Update Задача-2.3.c

--- Lab-4_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-2.3.c
+++ Lab-4_Bebutov_Hudayberdi_kurs-1_IWT-1/Задачи/Задача-2.3.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
-
-enum Weekday { Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье };
+#include <ctype.h>
 
 int main() {
-    for (enum Weekday day = Понедельник; day <= Воскресенье; day++) {
-        printf("%d ", day);
+    char str[256];
+    unsigned char c;
+    int i;
+    
+    printf("Введите строку: ");
+    fgets(str, sizeof(str), stdin);
+    
+    printf("Результат классификации:\n");
+    
+    for (i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        c = (unsigned char)str[i];
+        printf("'%c' -> ", str[i]);
+        
+        if (isdigit(c)) {
+            printf("цифра\n");
+        } else if (isalpha(c)) {
+            printf("буква\n");
+        } else if (isspace(c)) {
+            printf("пробел\n");
+        } else if (ispunct(c)) {
+            printf("пунктуация\n");
+        } else {
+            printf("другое\n");
+        }
     }
-    printf("\n");
+    
     return 0;
 }
